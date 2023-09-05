7f6e9fba010d5e87fb8de2069053e013f9fb5d29
Grzegorz Rusin <grzegorz.rusin@databricks.com>
2023-09-05 14:22:52 +0200

    white space

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-terraform -chdir=aad init  &&
+terraform -chdir=aad init &&
 terraform -chdir=aad apply --auto-approve > /dev/null &&
 terraform -chdir=aad output -json > .aad_state.json &&
 terraform init &&
